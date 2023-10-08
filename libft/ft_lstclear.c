093ffbe6b6886a201b645084563011a4f7fa4496
pedrosm <psotto-m@student.42porto.com>
2023-10-08 15:22:58 +0100

    norminetted to norminette 3.3.53

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,14 +6,14 @@
 /*   By: psotto-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 00:50:34 by psotto-m          #+#    #+#             */
-/*   Updated: 2022/11/13 00:54:27 by psotto-m         ###   ########.fr       */
+/*   Updated: 2023/10/08 15:22:38 by psotto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
-{	
+{
 	t_list	*temp;
 
 	if (!lst || !del)
